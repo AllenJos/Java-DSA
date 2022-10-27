d533e292a804e8d22e1c4dfdb34c20680ba4c998
Allen Joseph <allenjos1996@gmail.com>
2022-10-27 16:28:27 +0200

    Generate Numbers with Given Digits

--- src/main/java/GenerateNumbersWith5_6.java
+++ src/main/java/GenerateNumbersWith5_6.java
@@ -1,13 +1,15 @@
 import java.util.ArrayDeque;
 import java.util.Queue;
 
+//code to generate first n numbers containing two given digits
+//using the concept level order traversal using the Queue data structure
 public class GenerateNumbersWith5_6 {
     public static void main(String[] args) {
         Queue<String> queue = new ArrayDeque<>();
         queue.offer("5");
         queue.offer("6");
-        int n=12;
-
+        int n=10;
+        //expected output: 5 6 55 56 65 66 555 556 565 566
         for(int i=0; i<n; i++)
         {
             String current = queue.poll();
