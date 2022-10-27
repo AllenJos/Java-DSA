aa17dc33ef7061d2b1a9ad440385f8ef284d7b7a
Allen Joseph <allenjos1996@gmail.com>
2022-10-27 16:31:46 +0200

    Purchase Maximum Items

--- src/main/java/PurchaseMaxItems.java
+++ src/main/java/PurchaseMaxItems.java
@@ -1,13 +1,17 @@
 import java.util.*;
+
+//Given an array with n elements and a Sum
+//code for finding the max items we can purchase from the array with the Sum given
 public class PurchaseMaxItems {
     public static void main(String[] args) {
 //        List<Integer> arr = Arrays.asList(20, 10, 5,30, 100);
-        List<Integer> arr = Arrays.asList(111, 12, 5, 1, 200);
+        List<Integer> arr = Arrays.asList(111, 12, 5, 1, 200); //expected output: 2
         int sum=10;
         int items = purchaseMax(arr, sum);
         System.out.println(items);
     }
 
+    //the idea is to use a Priority Queue which implements Min-Heap
     public static int purchaseMax(List<Integer> arr, int sum){
         if(arr.size()==0)
             return 0;
@@ -24,8 +28,8 @@ public class PurchaseMaxItems {
         //check if the value of (itemSum+current smallest element) in
         //the priority queue is smaller than the sum
         while(itemSum+pq.peek()<=sum && pq.size()>0){
-            //if yes then add it to the itemSum and
-            //increment the itemCount
+            //if yes then add it to the itemSum and remove it from the priority queue
+            //and increment the itemCount
                 itemSum += pq.poll();
                 itemCount++;
         }
