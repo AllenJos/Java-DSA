0b53b95fedda42d5313a9add13b316237407ce1e
Allen Joseph <allenjos1996@gmail.com>
2022-10-29 14:15:22 +0200

    Print Repeating Elements in an Array.

---
+++ src/main/java/PrintRepeatingElements.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+
+//simple code to print Repeating Elements in an array.
+public class PrintRepeatingElements {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 8, 7, 7, 10}; //expected outcome: 7 10
+        printRepeating(arr);
+    }
+
+    public static void printRepeating(int[] arr){
+        HashSet<Integer> repeating = new HashSet<>();
+
+        for(int i=0; i<arr.length; i++){
+            if(repeating.contains(arr[i])){
+                System.out.print(arr[i]+" ");
+            }
+            repeating.add(arr[i]);
+        }
+    }
+}
