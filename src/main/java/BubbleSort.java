7ce32b8eeb75ea9363ad0410e1f484b4da2833f3
Allen Joseph <allenjos1996@gmail.com>
2022-12-01 15:09:37 +0100

    Code implementing Bubble Sort.

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -8,7 +8,7 @@ public class BubbleSort {
     }
 
     private static void bubbleSort(int[] arr) {
-        for(int i=0; i<arr.length; i++){
+        for(int i=0; i<arr.length-1; i++){
             for(int j=0; j<arr.length-i-1; j++){
                 if(arr[j]>arr[j+1]){
                     swapElements(arr, j, j+1);
