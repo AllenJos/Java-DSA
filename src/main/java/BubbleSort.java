30a7dd6c6aa547063038b81b816dca9c38d2166b
Allen Joseph <allenjos1996@gmail.com>
2022-12-01 15:02:12 +0100

    Code implementing Bubble Sort.

---
+++ src/main/java/BubbleSort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+//Code implementing Bubble Sort
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 10, 3, 7, 6, 9}; //expected outcome: [2, 3, 5, 6, 7, 9, 10]
+        bubbleSort(arr);
+    }
+
+    private static void bubbleSort(int[] arr) {
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    swapElements(arr, j, j+1);
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void swapElements(int[] arr, int j, int i) {
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+}
