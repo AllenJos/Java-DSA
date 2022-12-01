d6ab1c574e016fd3bf28c9949522b5e09586f613
Allen Joseph <allenjos1996@gmail.com>
2022-12-01 15:22:21 +0100

    Code implementing Bubble Sort(Optimized)

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,19 +1,25 @@
 import java.util.Arrays;
 
-//Code implementing Bubble Sort
+//Code implementing Bubble Sort which has been optimized, in case,
+// the array gets sorted midway or is already sorted.
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = {5, 2, 10, 3, 7, 6, 9}; //expected outcome: [2, 3, 5, 6, 7, 9, 10]
+        int[] arr = {12 ,5, 2, 10, 3, 7, 6, 9}; //expected outcome: [2, 3, 5, 6, 7, 9, 10, 12]
+//        int[] arr = {1, 2, 3, 4};
         bubbleSort(arr);
     }
 
     private static void bubbleSort(int[] arr) {
         for(int i=0; i<arr.length-1; i++){
+            boolean swapped = false;
             for(int j=0; j<arr.length-i-1; j++){
                 if(arr[j]>arr[j+1]){
                     swapElements(arr, j, j+1);
+                    swapped = true;
                 }
             }
+            if(!swapped)
+                break;
         }
         System.out.println(Arrays.toString(arr));
     }
